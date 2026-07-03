db542fe5ae82af86091cbb364c382df882574abc
Evan Trowbridge <evan@trowbridge.tech>
2026-07-02 21:34:30 -0400

    remove unused Docker Compose and SQL files, improved db creation

--- volumes/db/storage.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE storage.buckets ADD COLUMN "public" boolean default false;
-update storage.buckets set public = true WHERE id = 'avatars' OR id = 'groups' OR id = 'lists' OR id = 'items';
